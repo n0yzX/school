dbd55eed522d740df8531261c2b1623f6496d82c
vin <fischer.k92@gmail.com>
2022-11-09 08:40:36 +0100

    fakultät schleife

--- 11/fakultät.py
+++ 11/fakultät.py
@@ -1,7 +1,23 @@
+zahlinput = 8
+
 def reku_fak(zahl):
     if zahl == 1:
         return 1
     else:
         return zahl * reku_fak(zahl-1)
 
-print(reku_fak(4))
+print(reku_fak(zahlinput))
+
+def iter_fak (zahl):
+    if zahl == 1:
+        return 1
+
+    else:
+        sum = 1
+        for n in range(2, zahl +1):
+            sum *= n
+        
+        return sum
+
+
+print(iter_fak(zahlinput))
