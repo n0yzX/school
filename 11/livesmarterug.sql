173f7bd69d34593778e6c522d30c5366cd82be9d
vin <fischer.k92@gmail.com>
2023-03-10 11:28:51 +0100

    create table auftrag

--- 11/livesmarterug.sql
+++ 11/livesmarterug.sql
@@ -2,12 +2,22 @@ DROP Database livesmart;
 CREATE DATABASE livesmart;
 USE livesmart;
 
-CREATE Table kunde (
+CREATE TABLE kunde (
     kundennr INT PRIMARY KEY AUTO_INCREMENT,
-    vorname 
-    nachname
-    telefon
-    strasse
-    plz
-    ort
-)
+    vorname VARCHAR(30) NOT NULL,
+    nachname VARCHAR(30)NOT NULL,
+    telefon VARCHAR(30) NOT NULL,
+    strasse VARCHAR(30) NOT NULL,
+    plz INT(5) NOT NULL,
+    ort VARCHAR(20)NOT NULL
+);
+
+CREATE TABLE auftrag (
+    auftragsnr INT PRIMARY KEY AUTO_INCREMENT,
+    kundennr INT,
+    datum DATE NOT NULL,
+    frist DATE NOT NULL,
+    erledigt BOOLEAN NOT NULL,
+    bezahlt BOOLEAN NOT NULL,
+    FOREIGN KEY (kundennr) REFERENCES kunde (kundennr)
+);
