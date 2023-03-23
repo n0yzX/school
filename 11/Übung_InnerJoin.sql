b5008f43c66f6dd212fb352ef60350036bc992d2
vin <fischer.k92@gmail.com>
2023-03-23 08:18:08 +0100

    InnerJoin Ü1

--- 11/Übung_InnerJoin.sql
+++ 11/Übung_InnerJoin.sql
@@ -11,3 +11,19 @@ FROM auftrag au
 INNER JOIN auftragsposition ap ON au.auftragsnummer = ap.auftragsnummer
 INNER JOIN artikel a ON a.artikelnummer = ap.artikelnummer
 WHERE au.auftragsnummer = 201;
+
+Aufgabe 3
+
+SELECT a.artikelnummer, a.artikelname ap.menge, k.vorname, k.nachname, au.auftragsnummer
+FROM auftrag au
+INNER JOIN auftragsposition ap ON au.auftragsnummer = ap.auftragsnummer
+INNER JOIN artikel a ON a.artikelnummer = ap.artikelnummer
+INNER JOIN kunde k ON au.kundennummer = k.kundennummer
+WHERE au.auftragsnummer = 201;
+
+Aufgabe 4
+SELECT a.artikelnummer, a.artikelname, l.lieferantennamen, h.herstellernamen
+FROM artikel a
+INNER JOIN artikellieferant al ON a.artikelnummer = al.artikelnummer
+INNER JOIN lieferant l ON al.lieferantennummer = l.lieferantennummer
+INNER JOIN hersteller h ON h.herstellernummer = a.herstellernummer;
